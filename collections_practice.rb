a4664aceff7a19f955359ec2e86b9cafa78a92b3
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-24 22:27:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -106,10 +106,10 @@ def organize_schools(schools)
   schools.each_with_index do |school, i|
     if organized.include?(school[i][:location])
       organized[school[1][:location]].push(school[0])
+      binding.pry
     else  organized.include?(school[i][:location])
       organized[school[1][:location]] = Array.new
       organized[school[1][:location]].push(school[0])
-        binding.pry
     end
   end
 
