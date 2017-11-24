05f70f27e5db93d0af7b7bd5d6e96f4430e4d46e
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-24 17:58:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,88 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+  array.each do |word|
+    if word[0] != "r"
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  newArray = []
+  array.each do |word|
+    if word.include?("a")
+      newArray.push(word)
+    end
+  end
+  newArray
+end
+
+def first_wa(array)
+  array.each do |word|
+    if word[0] == "w"
+      if word[1] == "a"
+        return word
+      end
+    end
+  end
+end
+
+def remove_non_strings(array)
+  newArray = []
+  array.each do |element|
+    if element.class == String
+      newArray.push(element)
+    end
+  end
+  newArray
+end
+
+# def count_elements(array)
+#   newArray = []
+#   array.each do |obj|
+#     newArray.push(obj)
+#       #if newArray.length > 0
+#         newArray.each do |obj|
+#           if obj.include?(:count)
+#             obj[:count] += 1
+#           else
+#             obj[:count] = 1
+#           end
+#           binding.pry
+#         end
+#       #end
+#   end
+#   newArray.uniq
+# end
+
+def count_elements(array)
+  newArray = []
+  array.each do |obj|
+    newArray.push(obj)
+    obj[:count] = 0
+      #if newArray.length > 0
+        newArray.each do |obj|
+          if obj.include?(:count)
+            obj[:count] += 1
+          else
+            obj[:count] = 1
+          end
+          #binding.pry
+        end
+      #end
+  end
+  newArray.shift
+  return newArray
+end
+
+def merge_data(keys, data)
+keys[0].values.map.with_index do |key, i|
+  data[i].merge(key)
+  end
+end
+
+#add data into key
+#flatten
+#delete 1st element
